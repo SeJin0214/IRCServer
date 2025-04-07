754c665a53c5e416877038137a518c6b3d5a0062
Sejin Jeong <sejjeong@c2r10s5.42gyeongsan.kr>
2025-04-07 18:40:28 +0900

    feat: implement ErrorCommand

--- command/ErrorCommand.cpp
+++ command/ErrorCommand.cpp
@@ -6,19 +6,29 @@
 /*   By: sejjeong <sejjeong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 12:53:02 by sejjeong          #+#    #+#             */
-/*   Updated: 2025/04/07 18:25:09 by sejjeong         ###   ########.fr       */
+/*   Updated: 2025/04/07 18:39:06 by sejjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cassert>
+#include <cstring>
 #include "ErrorCommand.hpp"
 
 MessageBetch ErrorCommand::getMessageBetch(const Server& server, const int clientSocket, const char* buffer) const
 {
 	assert(buffer != NULL);
-	(void) server;
-	(void) clientSocket;
-	MessageBetch msg;
-	// 유효하지 않은 인자
-	return msg;
+	char temp[MAX_BUFFER] = { 0, };
+	std::strcpy(temp, buffer);
+	const char* command = std::strtok(temp, " ");
+	std::stringstream message;
+	
+	User user = server.findUser(clientSocket).getValue();
+	assert(server.findUser(clientSocket).hasSucceeded());
+	
+	message << ":" << server.getServerName() << " 421 " << user.getNickname() << " " << command 
+	<< " :Unknown command";
+	
+	MessageBetch messageBetch;
+	messageBetch.addMessage(clientSocket, message.str());
+	return messageBetch;
 }
